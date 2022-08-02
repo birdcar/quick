8807414c1b6ae335a8b23cdca385f69392e3c09b
Birdcar <434063+birdcar@users.noreply.github.com>
2022-08-02 12:00:43 -0500

    Adjust command API to match expected use

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -4,10 +4,11 @@ export default class Create extends Command {
   static description = "Create a new GitHub Action";
 
   static examples = [
+    '<%= config.bin %> <%= command.id %> add_to_project',
+    '<%= config.bin %> <%= command.id %> add_to_project -c',
+    '<%= config.bin %> <%= command.id %> add_to_project --composite',
     '<%= config.bin %> <%= command.id %> add_to_project --authorName="Mona Octocat" --authorEmail="mona.octocat@example.com"',
-    '<%= config.bin %> <%= command.id %> add_to_project -c --authorName="Mona Octocat" --authorEmail="mona.octocat@example.com"',
-    '<%= config.bin %> <%= command.id %> add_to_project --composite --authorName="Mona Octocat" --authorEmail="mona.octocat@example.com"',
-    '<%= config.bin %> <%= command.id %> add_to_project --authorName="Mona Octocat" --authorEmail="mona.octocat@example.com"',
+    '<%= config.bin %> <%= command.id %> add_to_project -g octocat',
   ];
 
   static flags = {
@@ -18,22 +19,28 @@ export default class Create extends Command {
     }),
     authorName: Flags.string({
       char: "a",
-      description: "The given name of the author",
-      required: true,
+      description: "The given name of the author (uses git config if not provided)",
     }),
     authorEmail: Flags.string({
       char: "e",
-      description: "The public email of the author",
-      required: true,
+      description: "The public email of the author (uses git config if not provided)",
+    }),
+    githubHandle: Flags.string({
+      char: 'g',
+      description: "The GitHub username of the author (uses origin remote if value isn't provided and remote is available)",
     }),
   };
 
-  static args = [{ name: "action_slug", required: true }];
+  static args = [
+    { name: "name", required: true },
+    { name: "inputs" },
+    { name: "outputs" },
+  ];
 
   public async run(): Promise<void> {
     const { args, flags } = await this.parse(Create);
 
-    this.log(`Project name: ${args.action_slug}`);
+    this.log(`Project name: ${args.name}`);
 
     if (flags.composite) {
       this.log(`you input --composite: ${flags.composite}`);
